152fd2ccb8f2db05842750975cdbfb291fd308e0
Francisco Gómez García <espectalll123@kydara.com>
2015-10-24 12:59:53 +0200

    Basic server

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 80));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use(express.static('htdocs'));
 
 app.listen(app.get('port'), function() {
     console.log('Running on port', app.get('port')); 
-}); 
+});
