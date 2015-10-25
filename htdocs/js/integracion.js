963b93d678c989e2f81cf2011f1f466949dcba89
Javier Gil Romero <yoruichi12@hotmail.com>
2015-10-25 13:11:52 +0100

    Arreglado commit

--- htdocs/js/integracion.js
+++ htdocs/js/integracion.js
@@ -1,20 +1,34 @@
-
+﻿
 var context;
-var desde,hasta;
+var desde, hasta;
 var lista;
 var costes;
-function pintarDispositivo(data){
-  var source = $("#dispositivos").html();
-  var template = Handlebars.compile(source);
-  context = data.attributes[0];
+function pintarDispositivo(data) {
+    var source = $("#dispositivos").html();
+    var template = Handlebars.compile(source);
+    context = data.attributes[0];
 
-  var theCompiledHtml = template(context);
+    var theCompiledHtml = template(context);
 
-  document.getElementById("consumo").innerHTML =totalConsumption(data.attributes[0].value);
-  $('.content-placeholder').html(theCompiledHtml);
+    $("#consumo").text(totalCost(data.attributes[0].value, costes) + "€ / hora");
+    $("#precioKwhActual").text(nowCost(costes).pvpc + "€");
+    $('.content-placeholder').html(theCompiledHtml);
 
 }
+
+function pintarAlertas() {
+    var source = $("#avisos").html();
+    var template = Handlebars.compile(source);
+    context = {
+        name: "Hola",
+        description: "Ninoninonino",
+        type: "success"
+    };
+    $('#alert-placeholder').html(template(context));
+}
+
 obtenerTodosLosDispositivos(pintarDispositivo);
+pintarAlertas();
 
 $(document).ready(function () {
 
@@ -27,57 +41,59 @@ $(document).ready(function () {
         };
     });
 
-  $( "#Desde" ).click(function() {
-      desde = document.getElementById("Desde").value;
-        //console.log(desde);
-      });
-  $( "#Hasta" ).click(function() {
-      hasta = document.getElementById("Hasta").value;
-      //  console.log(hasta);
-      });
-  $( "#calcular" ).click(function() {
-      device = document.getElementById("Dispositivo").value;
-      calculo = fringeBestCost(desde,hasta,costes);
-      //console.log(calculo);
-      });
+    $("#Desde").click(function () {
+        desde = document.getElementById("Desde").value;
+        console.log(desde);
+    });
+    $("#Hasta").click(function () {
+        hasta = document.getElementById("Hasta").value;
+        console.log(hasta);
+    });
+    $("#calcular").click(function () {
+        device = document.getElementById("Dispositivo").value;
+        calculo = fringeBestCost(desde, hasta, costes);
+        //console.log(calculo);
+    });
 
 });
-function preciosHoy(costes){
-  //console.log(costes);
+function preciosHoy(cost) {
+    costes = cost;
+    console.log(costes);
+    document.getElementById("mejor-franja").innerHTML = bestCostNow(costes).hour + ":00 - " + (bestCostNow(costes).hour + 1) + ":00";
 }
 obtenerPreciosHoy(preciosHoy);
-function listarDispositivo(data){
-  lista = data.attributes[0].value;
-  $.each(lista,function(i,item){
-    $("#Dispositivo").append($("<option>",{
-      value: item.name,
-      text: item.name
-    }))
-  }
-);
+function listarDispositivo(data) {
+    lista = data.attributes[0].value;
+    $.each(lista, function (i, item) {
+        $("#Dispositivo").append($("<option>", {
+            value: item.name,
+            text: item.name
+        }))
+    }
+  );
 
 }
 
 function encenderApagarClick(nombre, accion) {
-  //modificar context para encender o apagar
-  //pasar a context
+    //modificar context para encender o apagar
+    //pasar a context
 
     for (i in context.value) {
-      //console.log(context.value[i]);
-      if(context.value[i].name == nombre){
-        var encendido = !(context.value[i].metadatas[0].value);
+        //console.log(context.value[i]);
+        if (context.value[i].name == nombre) {
+            var encendido = !(context.value[i].metadatas[0].value);
 
-        encendido = encendido.toString();
-        context.value[i].metadatas[0].value = encendido;
+            encendido = encendido.toString();
+            context.value[i].metadatas[0].value = encendido;
 
-      }
+        }
     }
     if (accion == "encender")
         alert("encender: " + nombre);
     else
         alert("apagar: " + nombre);
     console.log(context);
-    encenderApagar(function () { }, context)
+    encenderApagar(function () { }, context);
 }
 obtenerTodosLosDispositivos(listarDispositivo);
-document.getElementById("mejor-franja").innerHTML = bestCostNow(costes);
+
